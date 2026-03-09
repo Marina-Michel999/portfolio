4f40e0a6f4b6c48a263a403a8927fbb5727d77c5
Marina-Michel999 <marinamichel@gmail.com>
2026-03-09 20:47:10 +0200

    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    HomeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
